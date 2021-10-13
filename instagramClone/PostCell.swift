9b4e040acf0a69264d022fe02155d44db2b28a64
jungtaelee0128 <jungtaelee0128@gmail.com>
2021-10-12 23:33:03 -0400

    first commit

---
+++ instagramClone/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  instagramClone
+//
+//  Created by Jung tae Lee on 10/12/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
